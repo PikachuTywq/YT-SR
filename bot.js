53e745bd88ecb58ac4cfa3ebcb73c2cdcc28200e
PikachuTywq <59270235+PikachuTywq@users.noreply.github.com>
2019-12-27 04:18:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'sets')) {
+if (message.content.startsWith(prefix + 'setT')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
