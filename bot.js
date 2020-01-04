99cdc2216409768751be28d2ca7f499a0c570261
PikachuTywq <59270235+PikachuTywq@users.noreply.github.com>
2020-01-04 07:37:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,14 +17,14 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + 'setw')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`:heart:`)
+    message.channel.sendMessage(`:white_check_mark:`)
 } 
 
  
   if (message.content.startsWith(prefix + 'setl')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`:heart:`)
+    message.channel.sendMessage(`:white_check_mark::`)
 } 
 
 
@@ -42,12 +42,12 @@ if (message.content.startsWith(prefix + 'setavatar')) {
 if (message.content.startsWith(prefix + 'sets')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
-    message.channel.sendMessage(`:heart:`)
+    message.channel.sendMessage(`:white_check_mark:`)
 } 
 if (message.content.startsWith(prefix + 'setg')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
-    message.channel.sendMessage(`:heart:`)
+    message.channel.sendMessage(`:white_check_mark:`)
 } 
 
 
